98c8b12f8a109972e91b609fd6551f508583f375
Yassir Lazreq <yassirlazre@gmail.com>
2026-02-27 14:34:36 +0000

    Add CategorySeeder to populate common expense categories

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -2,7 +2,8 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Category;
+use App\Models\Colocation;
 use Illuminate\Database\Seeder;
 
 class CategorySeeder extends Seeder
@@ -12,6 +13,27 @@ class CategorySeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $commonCategories = [
+            ['name' => 'Groceries', 'description' => 'Food and household groceries'],
+            ['name' => 'Utilities', 'description' => 'Electricity, water, gas, internet'],
+            ['name' => 'Rent', 'description' => 'Monthly rent and housing costs'],
+            ['name' => 'Transportation', 'description' => 'Fuel, public transport, and travel'],
+            ['name' => 'Maintenance', 'description' => 'Repairs and home maintenance'],
+            ['name' => 'Other', 'description' => 'Miscellaneous shared expenses'],
+        ];
+
+        Colocation::query()->each(function (Colocation $colocation) use ($commonCategories): void {
+            foreach ($commonCategories as $categoryData) {
+                Category::query()->firstOrCreate(
+                    [
+                        'colocation_id' => $colocation->id,
+                        'name' => $categoryData['name'],
+                    ],
+                    [
+                        'description' => $categoryData['description'],
+                    ]
+                );
+            }
+        });
     }
 }
